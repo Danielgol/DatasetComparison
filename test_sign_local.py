d29b8715c17bc16f17daaefd1f1ae3c297e931c0
Daniel Rosa <dan.rosa10@gmail.com>
2021-12-15 19:26:26 -0300

    first commit

--- test_sign_local.py
+++ test_sign_local.py
@@ -27,8 +27,6 @@ def calculate(hyps, refs):
     bleu = compute_cvpr_bleu(h, r)
     rouge_score = rouge.rouge(h, r)
 
-
-
     for i in range(len(h)):
         i_hyp = [h[i]]
         i_ref = [r[i]]
@@ -36,9 +34,8 @@ def calculate(hyps, refs):
         i_rouge = rouge.rouge(i_hyp, i_ref)
 
         print("hyp:",h[i],"\nref:",r[i])
-        print("Teste 2")
-        #print('performance: {:.2f} {}'
-        #    .format(i_rouge['rouge_l/f_score']*100 ,' '.join([str(b) for b in i_bleu])))
+        #print("Teste 2")
+        #print('performance: {:.2f} {}'.format(i_rouge['rouge_l/f_score']*100 ,' '.join([str(b) for b in i_bleu])))
         print("\n")
 
     #print('{} set has {} samples,\n'
@@ -64,10 +61,9 @@ def compute_cvpr_bleu(hyps, refs, max_order=4):
     bleu_all_orders = []
 
     for i in list(range(1, max_order+1)):
-        bleu, precisions, bp, ratio, translation_length, reference_length =
-        compute_bleu(tokenized_refs, tokenized_hyps, max_order=i)
+        bleu, precisions, bp, ratio, translation_length, reference_length = compute_bleu(tokenized_refs, tokenized_hyps, max_order=i)
         if i == 4:
-            print ('precisions: {}'.format([str(round(p*100, 2)) for p in precisions]))
+            #print ('precisions: {}'.format([str(round(p*100, 2)) for p in precisions]))
         bleu_all_orders.append(round(bleu * 100, 2))
 
     return bleu_all_orders
